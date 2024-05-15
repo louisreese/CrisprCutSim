939e9bcaa4e62d62d3a4eb4d1acf15e3d73392e5
louisreese <167224099+louisreese@users.noreply.github.com>
2024-05-15 18:09:16 +0200

    Update and rename main_cleavagesim.cpp to main.cpp
    filename change

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*
  *
- *  Simulate digestion of DNA by nucleases.
+ *  Simulate digestion of DNA or RNA by CRISPR nucleases.
  *
- *  main_cleavagesim.cpp
+ *  main.cpp
  *  
  *  Created by Louis Reese on 18.01.2021
  *  Copyright 2021 Louis Reese. All rights reserved.
